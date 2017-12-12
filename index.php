8e1fff649a8fd8e0f96086712a160671f17c5659
Anthony <anthony_bouillon@outlook.fr>
2017-12-12 15:07:57 +0100

    Les boucles

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Les boucles exercice 6</title>
+  </head>
+  <body>
+    <?php
+    $number1 = 20;
+    while($number1 >= 0){
+      echo '<p> On y arrive presque, plus que ' .$number1. ' pas</p>';
+      $number1--;
+    }
+     ?>
+  </body>
+</html>
